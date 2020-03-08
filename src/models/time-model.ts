dd4cdf567103fd7c9565708968d6659bfef89462
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-03-08 22:27:04 +0000

    Try to get time working through model

--- src/models/time-model.ts
+++ src/models/time-model.ts
@@ -1,18 +1,30 @@
 import { ITime } from "../interfaces/ITime";
+import { WorldTimeApiResponseSchema } from "./time-types";
 
 class TimeModel implements ITime {
     Time: string = "20:00";
 
-    getTime = async () => {
-
+    getTime = () => {
         const asyncFetchTime = async () => {
-            const response = await fetch("http://worldtimeapi.org/api/timezone/Europe/London");
-            return await response.json()
-        }
+            const response = await fetch(
+                "http://worldtimeapi.org/api/timezone/Europe/London"
+            );
+            return await response.json();
+        };
 
-        return asyncFetchTime
+        let asyncData;
+        asyncFetchTime
+            .call(this)
+            .then((json: WorldTimeApiResponseSchema) => (asyncData = json));
+
+        let data;
+        fetch("http://worldtimeapi.org/api/timezone/Europe/London")
+            .then(res => res.json())
+            .then((json: WorldTimeApiResponseSchema) => (data = json));
+
+        return "yeah I need to work on this in typescript kek";
         // return "21:00"
-    }
+    };
 
     //getRegions
 }
