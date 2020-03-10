a611edc0298a797489a3f244a25d083675992b2b
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-03-10 21:38:13 +0000

    Convert unixtime to something more readable

--- src/components/ClockFace.tsx
+++ src/components/ClockFace.tsx
@@ -6,9 +6,21 @@ type Props = {
     time: WorldTimeApiResponseSchema;
 };
 
+/**
+ * Entirely relies on a unixtime property existing on the time object
+ */
 const ClockFace = (props: Props) => {
     if (props.time) {
-        return <div>{props.time.unixtime}</div>;
+        const { unixtime } = props.time;
+
+        const date = new Date(unixtime * 1000);
+        const hours = date.getHours();
+        const mins = "0" + date.getMinutes();
+        const secs = "0" + date.getSeconds();
+        const formattedTime =
+            hours + ":" + mins.substr(-2) + ":" + secs.substr(-2);
+
+        return <div>{formattedTime}</div>;
     }
 
     return <div>Fetching time...</div>;
