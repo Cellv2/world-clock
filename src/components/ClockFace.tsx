55f42538eb26bad31a0d31657349ba914f1df1db
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-03-11 20:09:25 +0000

    Keep coding style consistent

--- src/components/ClockFace.tsx
+++ src/components/ClockFace.tsx
@@ -10,22 +10,21 @@ type Props = {
  * Entirely relies on a unixtime property existing on the time object
  */
 const ClockFace = (props: Props) => {
-    if (props.time) {
-        const { unixtime, raw_offset, dst_offset } = props.time;
-
-        const adjustedTime = unixtime + raw_offset + (dst_offset || 0);
-
-        const date = new Date(adjustedTime * 1000);
-        const hours = date.getHours();
-        const mins = "0" + date.getMinutes();
-        const secs = "0" + date.getSeconds();
-        const formattedTime =
-            hours + ":" + mins.substr(-2) + ":" + secs.substr(-2);
-
-        return <div>{formattedTime}</div>;
+    if (!props.time) {
+        return <div>Fetching time...</div>;
     }
 
-    return <div>Fetching time...</div>;
+    const { unixtime, raw_offset, dst_offset } = props.time;
+
+    const adjustedTime = unixtime + raw_offset + (dst_offset || 0);
+
+    const date = new Date(adjustedTime * 1000);
+    const hours = date.getHours();
+    const mins = "0" + date.getMinutes();
+    const secs = "0" + date.getSeconds();
+    const formattedTime = hours + ":" + mins.substr(-2) + ":" + secs.substr(-2);
+
+    return <div>{formattedTime}</div>;
 };
 
 export default ClockFace;
